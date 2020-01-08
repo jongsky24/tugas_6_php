577691508034a507f6be912f22476d08ccbda11f
Muhammad Ridwan R <jongsky_1821@yahoo.com>
2020-01-08 23:11:31 +0700

    Tugas 6_2 PHP
    Pengumpulan Tugas untuk niomic

---
+++ tugas6_2.php
@@ -0,0 +1,17 @@
+<?php
+$hj = 15000;
+$hd = 2000;
+$jj = 5 * 6;
+$thj = $jj * $hj;
+$jd = 2 * $hd;
+$dj = $thj + $jd;
+
+echo "=================================================================";
+echo "<br>";
+echo "Harga Jambu = Rp $hj / Kg";
+echo "<br>";
+echo "Harga Kardus = Rp $hd / pcs";
+echo "<br>";
+echo "Total Penjualan (Dus dan Jambu) = Rp $dj";
+echo "<br>";
+echo "=================================================================";
